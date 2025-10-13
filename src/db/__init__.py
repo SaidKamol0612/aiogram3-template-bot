ba69f6c867b5a320fd495230d8cbfb64bd40ec43
Saidkamol Mirsaidov <kamolsaid2006@gmail.com>
2025-10-13 16:24:25 +0500

    refactor: update helper and running

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,5 +1,3 @@
-__all__ = (
-    "db_helper",
-)
+__all__ = ("db_helper",)
 
 from .helper import db_helper
--- src/run.py
+++ src/run.py
@@ -1,52 +1,37 @@
 import logging
 import asyncio
 
-from bot import start_bot
-from db import db_helper
+from bot import main
 from core.config import settings
+from db import db_helper
 
-# -------------------------------
-# Logger for this module
-# -------------------------------
 log = logging.getLogger(__name__)
 
 
-async def run():
-    """
-    Main async function to initialize the database and start the bot.
-    Ensures proper cleanup of database connections on exit.
-    """
-
-    # Initialize database
+async def run_bot():
     await db_helper.init_db()
 
     try:
-        await start_bot()
+        await main()
     except asyncio.CancelledError:
-        # Clean exit without stacktrace on task cancellation
         logging.info("Bot task cancelled. Exiting gracefully...")
     except KeyboardInterrupt:
-        # Clean exit on Ctrl+C
         logging.info("Bot stopped manually with Ctrl+C.")
     except Exception as e:
         logging.exception("Bot stopped with unexpected exception: %s", e)
     finally:
-        # Always dispose the DB engine
         await db_helper.dispose()
 
 
 if __name__ == "__main__":
-    # Configure logging according to settings
     logging.basicConfig(
-        filename=settings.logging.log_file if not settings.bot.debug else None,
+        filename=settings.logging.log_file if not settings.debug else None,
         format=settings.logging.log_format,
         datefmt=settings.logging.log_date_format,
         level=settings.logging.log_level_value,
     )
 
     try:
-        # Run the async main function
-        asyncio.run(run())
+        asyncio.run(run_bot())
     except KeyboardInterrupt:
-        # This catches Ctrl+C during asyncio.run
         logging.info("Bot stopped manually with Ctrl+C at main level.")
